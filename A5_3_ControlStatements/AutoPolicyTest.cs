87898d9571eadf3a1d2d0da4c5128298fe864ce0
Tim0Tim <graden.bishop2995@omail.otech.edu>
2022-03-24 11:08:01 -0600

    completed and extreamly simplified.

--- A5_3_ControlStatements/AutoPolicyTest.cs
+++ A5_3_ControlStatements/AutoPolicyTest.cs
@@ -8,23 +8,21 @@ namespace A5_3_ControlStatements
 {
     class AutoPolicyTest
     {
-        static void Main()
+        static public void Main()
         {
-            AutoPolicy policy1 = new AutoPolicy(11111111, "Toyota Camry", "NJ");
-            AutoPolicy policy2 = new AutoPolicy(22222222, "Ford Fusion", "ME");
+            Console.WriteLine("Please incert state abreviation...(all caps are required)");
+            string Test = Console.ReadLine();
 
-            PolicyInNoFaultState(policy1);
-            PolicyInNoFaultState(policy2);
-        }
+            if (Test == "MA" || Test == "NJ" || Test == "NY" || Test == "PA" || Test == "CT" || Test == "ME" || Test == "NH" || Test == "PA" || Test == "VT")
+            {
+                Console.WriteLine($"{Test} is a No Fault State");
+            }
+            else
+            {
+                Console.WriteLine($"{Test} is Not a 'No Fault State' or is an invalid response");
+            }
 
-        public static void PolicyInNoFaultState(AutoPolicy policy)
-        {
-            Console.WriteLine("The auto policy:");
-            Console.Write($"Account #: {policy.AccountNumber}; ");
-            Console.WriteLine($"Car: {policy.MakeAndModel};");
-            Console.Write($"State {policy.State} ");
-            Console.Write($"{(policy.IsNoFaultState ? "is": "is not")}");
-            Console.WriteLine(" a no-fault state\n");
+            
         }
     }
 }
